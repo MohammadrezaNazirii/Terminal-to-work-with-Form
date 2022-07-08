5451ad4468b1653320e35ec54c6eb16d72a70214
Mohammadreza <mohammadrezanaziri0@gmail.com>
2022-07-08 23:47:28 +0430

    Add the ability to create a new file and check the existence of the file

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <string.h>
 #include "myHeader.h"
 
 int main() {
@@ -16,11 +17,52 @@ int main() {
                 c = getchar();
                 emptyBuffer();
                 if (c == '1' || c == '2') {
+                    int x, y;
+                    char name[100], name_for_print[100];
                     if (c == '1'){//New
-
+                        system("cls");
+                        while (1) {
+                            system("cls");
+                            printf("Please enter a name for your form... ");
+                            scanf("%s", name);
+                            strcpy(name_for_print, name);
+                            emptyBuffer();
+                            strcat(name, ".txt");
+                            if (!exist_file(name))
+                                break;
+                            else {
+                                printf("You have a form with this name!\nDo you want to rewrite it?\nType 1 if you want to rewrite it or type any character to enter new name\n");
+                                c = getchar();
+                                emptyBuffer();
+                                if (c == '1')
+                                    break;
+                            }
+                        }
+                        printf("Please enter dimensions of form separated by space(First number for width and Second number for height)... ");
+                        scanf("%d %d", &x, &y);
+                        emptyBuffer();
+                        allocate_lines(x, y);
+                        initialize_lines(x, y);
+                        save_file(name, x, y);
                     }else{//Edit
-
+                        system("cls");
+                        while(1){
+                            system("cls");
+                            printf("Please enter the name of your form... ");
+                            scanf("%s", name);
+                            strcpy(name_for_print, name);
+                            emptyBuffer();
+                            strcat(name, ".txt");
+                            if(exist_file(name))
+                                break;
+                            else {
+                                printf("Can't find form with this name.\nTry again!");
+                                Sleep(2500);
+                            }
+                        }
                     }
+
+
                 }
                 else if (c == '3') {
                     system("cls");
--- myHeader.h
+++ myHeader.h
@@ -1,3 +1,5 @@
+char** lines;
+
 void emptyBuffer(){
     char c;
     while (1){
@@ -21,4 +23,56 @@ void invalid_input(){
 void print_design_menu(){
     printf("1.New\n2.Edit\n3.Back/Exit\n");
     printf("What do you want to do? ");
+}
+
+int exist_file(char* name){
+    FILE* f = fopen(name, "r");
+    if(f == NULL)
+        return 0;
+    fclose(f);
+    return 1;
+}
+
+void allocate_lines(int x, int y){
+    lines = (char**) malloc(y*sizeof(char*));
+    for(int i=0;i<y;i++)
+        lines[i] = (char*) malloc((x+2) * sizeof(char));
+}
+
+char* create_line(int x, int mode){
+    char* arr = malloc((x+1) * sizeof(char));
+    if(mode == 0){
+        arr[0] = 'x';
+        for(int i=1;i<x-1;i++)
+            strcat(arr, "=");
+        arr[x-1] = 'x';
+        arr[x] = '\n';
+        arr[x+1] = '\0';
+    }else{
+        arr[0] = '|';
+        for(int i=1;i<x-1;i++)
+            strcat(arr, " ");
+        arr[x-1] = '|';
+        arr[x] = '\n';
+        arr[x+1] = '\0';
+    }
+    return arr;
+}
+
+void initialize_lines(int x, int y){
+    for(int i=0;i<y;i++){
+        if (i == 0 || i == y-1){
+            strcpy(lines[i], create_line(x, 0));
+        }else{
+            strcpy(lines[i], create_line(x, 1));
+        }
+    }
+}
+
+void save_file(char* name, int x, int y){
+    FILE* f = fopen(name, "w");
+    fprintf(f, "%d  %d\n", x, y);
+    for(int i=0;i<y;i++)
+        fprintf(f, "%s", lines[i]);
+    fclose(f);
 }
